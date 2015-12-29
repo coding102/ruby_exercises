e6d65f79fa68dab3294011ab0418caa750f91095
Marck <mercercountynj@gmail.com>
2015-12-29 17:17:34 -0500

    homework update

--- calculator.rb
+++ calculator.rb
@@ -1,59 +1,117 @@
-#list your choices
-puts "What do you want to do 
-    0 - [exit],  
-    1 - [add], 
-    2 - [subtract], 
-    3 - [multiply], 
-    4 - [divide]"
-response = gets.chomp
-
-case response.downcase
+class Calculator
     
-#Addition    
-when '1'
-    def addition_function
-        puts "add which numbers?"
-            n1 = gets.to_i #to integer
-            n2 = gets.to_i #to integer
-            answer = n1 + n2
-            puts "The sum is... #{n1} + #{n2} = #{answer}"
-end
-addition_function()
-
-
-#Subtract
-when '2'
-    def subtraction_function
-         puts "subtract which numbers?"
-            n1 = gets.to_i #to integer
-            n2 = gets.to_i #to integer
-            answer = n1 - n2
-            puts "The subtraction is... #{n1} - #{n2} = #{answer}"
-end
-subtraction_function()
-#Multiply
-when '3'
-    def multiplication_function
-        puts "multiply which numbers?"
-            n1 = gets.to_i #to integer
-            n2 = gets.to_i #to integer
-            answer = n1 * n2
-            puts "The multiplication is... #{n1} * #{n2} = #{answer}"
-end
-multiplication_function()
-
-
-#Division
-when '4'
-    def division_function
-        puts "divide which numbers?"
-            n1 = gets.to_i #to integer
-            n2 = gets.to_i #to integer
-            answer = n1 / n2
-            puts "The division is... #{n1} / #{n2} = #{answer}"
+    
+    
+    def initialize(value1,value2)
+        @value1 = value1
+        @value2 = value2
     end
-division_function()
-
-else '0'
-    puts "Bye!"
+    
+    
+    
+    def add
+        puts @value1 + @value2
+    end
+    
+    
+  
+    def subtract
+        puts @value1 - @value2
+    end
+    
+    
+    
+    def multiply
+        puts @value1 * @value2
+    end
+    
+    
+    
+    def divide
+        puts @value1 / @value2
+    end
+    
+    
+    
 end
+
+addition = Calculator.new(2,4)
+subtraction = Calculator.new(2,4)
+multiplication = Calculator.new(2,4)
+division = Calculator.new(4,2)
+
+addition.add
+subtraction.subtract
+multiplication.multiply
+division.divide
+
+
+  
+    
+    
+    
+    
+    
+#    
+#
+#
+#
+##list your choices
+#puts "What do you want to do 
+#    0 - [exit],  
+#    1 - [add], 
+#    2 - [subtract], 
+#    3 - [multiply], 
+#    4 - [divide]"
+#response = gets.chomp
+#
+#case response.downcase
+#    
+##Addition    
+#when '1'
+#    def addition_function
+#        puts "add which numbers?"
+#            n1 = gets.to_i #to integer
+#            n2 = gets.to_i #to integer
+#            answer = n1 + n2
+#            puts "The sum is... #{n1} + #{n2} = #{answer}"
+#end
+#addition_function()
+#
+#
+##Subtract
+#when '2'
+#    def subtraction_function
+#         puts "subtract which numbers?"
+#            n1 = gets.to_i #to integer
+#            n2 = gets.to_i #to integer
+#            answer = n1 - n2
+#            puts "The subtraction is... #{n1} - #{n2} = #{answer}"
+#end
+#subtraction_function()
+##Multiply
+#when '3'
+#    def multiplication_function
+#        puts "multiply which numbers?"
+#            n1 = gets.to_i #to integer
+#            n2 = gets.to_i #to integer
+#            answer = n1 * n2
+#            puts "The multiplication is... #{n1} * #{n2} = #{answer}"
+#end
+#multiplication_function()
+#
+#
+##Division
+#when '4'
+#    def division_function
+#        puts "divide which numbers?"
+#            n1 = gets.to_i #to integer
+#            n2 = gets.to_i #to integer
+#            answer = n1 / n2
+#            puts "The division is... #{n1} / #{n2} = #{answer}"
+#    end
+#division_function()
+#
+#else '0'
+#    puts "Bye!"
+#end
--- elevator.rb
+++ elevator.rb
@@ -1,21 +1,50 @@
-class Building
-    def elevator
-    puts "floor " + rand(10).to_s 
+class Elevator
+  attr_accessor :floor
+  def greet(floor)
+    floor = floor.to_i
+    if floor == 0
+        puts 'floor one is the lowest we go'
+    elsif floor < 50 || floor < 1
+      puts "Welcome to floor # #{floor}"
+    else
+      puts "#{floor} is not a floor"
     end
+  end
+end
+elev = Elevator.new
+choice = false
+while choice == false
+    print 'Which floor would you like to go to?(type quit to exit)'
+  prompt = gets.chomp.downcase
+  if prompt == 'quit'
+    choice = true
+  else
+    elev.greet(prompt)
+  end
 end
 
-my_building = Building.new # variable
-my_building.elevator # variable
-   
-puts "what floor are you going to?"
-    choice = gets.chomp.to_i # to integer
-  
-floors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # floor range
-  
-    if choice < 1 || choice > 10
-        puts "the elevator doesn't go to floor #{choice}!" # outside parameter settings
-    elsif choice == 1
-        puts "You are now on the ground floor, good bye" # 1st floor exit
-    else
-        puts "You are now on floor #{floors[choice-1]}" # arrival 
-  end
+
+
+
+
+#class Building
+#    def elevator
+#    puts "floor " + rand(10).to_s 
+#    end
+#end
+#
+#my_building = Building.new # variable
+#my_building.elevator # variable
+#   
+#puts "what floor are you going to?"
+#    choice = gets.chomp.to_i # to integer
+#  
+#floors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # floor range
+#  
+#    if choice < 1 || choice > 10
+#        puts "the elevator doesn't go to floor #{choice}!" # outside parameter settings
+#    elsif choice == 1
+#        puts "You are now on the ground floor, good bye" # 1st floor exit
+#    else
+#        puts "You are now on floor #{floors[choice-1]}" # arrival 
+#  end
